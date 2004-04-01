df4588a947eaee1303d87558b95638d7f51020df
Jesse Wilson <jesse@swank.ca>
2004-04-01 03:40:56 +0000

    added test to verify that the tree accepts multiple copies of the same object Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@125 696c4f5c-992c-4387-df9e-f6c076136008

--- source/com/odellengineeringltd/glazedlists/util/test/IndexedTreeTest.java
+++ source/com/odellengineeringltd/glazedlists/util/test/IndexedTreeTest.java
@@ -69,4 +69,70 @@ public class IndexedTreeTest extends TestCase {
         // verify the lists are equal
         assertEquals(controlList, indexedTreeList);
     }
+
+
+    /**
+     * Tests to verify that the IndexedTree is consistent with multiple
+     * entries that have the same value.
+     */
+    public void testEqualValues() {
+        IndexedTree indexedTree = new IndexedTree(new ComparableComparator());
+        
+        int ACount = 0;
+        int BCount = 0;
+        int CCount = 0;
+        int DCount = 0;
+        int ECount = 0;
+        
+        // populate the list with 100 B's and 100 D's
+        while(BCount < 100 || DCount < 100) {
+            indexedTree.addByNode("B");
+            BCount++;
+            indexedTree.addByNode("D");
+            DCount++;
+        }
+        
+        // add 100 A's, 100 C's and 100 E's in random order
+        while(ACount < 100 || CCount < 100 || ECount < 100) {
+            int letter = random.nextInt(3);
+            
+            if(letter == 0 && ACount < 100) {
+                indexedTree.addByNode("A");
+                ACount++;
+            } else if(letter == 1 && CCount < 100) {
+                indexedTree.addByNode("C");
+                CCount++;
+            } else if(letter == 2 && ECount < 100) {
+                indexedTree.addByNode("E");
+                ECount++;
+            }
+        }
+        
+        // remove the A's, C's and E's in random order
+        while(ACount > 0 || CCount > 0 || ECount > 0) {
+            int letter = random.nextInt(3);
+            
+            if(letter == 0 && ACount > 0) {
+                indexedTree.getNode("A").removeFromTree();
+                ACount--;
+            } else if(letter == 1 && CCount > 0) {
+                indexedTree.getNode("C").removeFromTree();
+                CCount--;
+            } else if(letter == 2 && ECount > 0) {
+                indexedTree.getNode("E").removeFromTree();
+                ECount--;
+            }
+        }
+        
+        // verify the list contains only the original 100 Bs and 100 Ds
+        List indexedTreeList = new ArrayList();
+        for(Iterator i = indexedTree.iterator(); i.hasNext(); ) {
+            IndexedTreeNode node = (IndexedTreeNode)i.next();
+            if(node.getValue().equals("B")) BCount--;
+            else if(node.getValue().equals("D")) DCount--;
+            else fail();
+        }
+        assertEquals(BCount, 0);
+        assertEquals(DCount, 0);
+    }
 }
