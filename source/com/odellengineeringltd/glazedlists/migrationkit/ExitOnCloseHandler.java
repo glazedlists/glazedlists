19621373e670f7936512071d39278f46a57992c1
Jesse Wilson <jesse@swank.ca>
2004-05-27 21:01:51 +0000

    moved this to migration Issue number: 20 Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@177 696c4f5c-992c-4387-df9e-f6c076136008

--- source/com/odellengineeringltd/glazedlists/migrationkit/ExitOnCloseHandler.java
+++ source/com/odellengineeringltd/glazedlists/migrationkit/ExitOnCloseHandler.java
@@ -4,7 +4,7 @@
  *
  * COPYRIGHT 2003 O'DELL ENGINEERING LTD.
  */
-package com.odellengineeringltd.glazedlists.util;
+package com.odellengineeringltd.glazedlists.migrationkit;
 
 // for being a window adapter
 import java.awt.*;
