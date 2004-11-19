36031fc8e6db6953a09b19cabb1a22b6d4796797
Jesse Wilson <jesse@swank.ca>
2004-11-19 02:41:45 +0000

    only one mouse button
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@445 696c4f5c-992c-4387-df9e-f6c076136008

--- source/ca/odell/glazedlists/swing/TableComparatorChooser.java
+++ source/ca/odell/glazedlists/swing/TableComparatorChooser.java
@@ -182,7 +182,7 @@ public final class TableComparatorChooser extends MouseAdapter implements TableM
      * @see <a href="http://java.sun.com/docs/books/tutorial/uiswing/components/table.html#sorting">The Java Tutorial</a>
      */
     public void mouseClicked(MouseEvent e) {
-        if(e.getButton() == MouseEvent.BUTTON1) return;
+        if(e.getButton() != MouseEvent.BUTTON1) return;
         
         TableColumnModel columnModel = table.getColumnModel();
         int viewColumn = columnModel.getColumnIndexAtX(e.getX());
