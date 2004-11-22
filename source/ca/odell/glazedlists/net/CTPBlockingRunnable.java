1f4b97a2495fcae994d76fa958957625aa2798f8
Jesse Wilson <jesse@swank.ca>
2004-11-22 22:59:15 +0000

    more threading code
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@456 696c4f5c-992c-4387-df9e-f6c076136008

---
+++ source/ca/odell/glazedlists/net/CTPBlockingRunnable.java
@@ -0,0 +1,49 @@
+/**
+ * Glazed Lists
+ * http://glazedlists.dev.java.net/
+ *
+ * COPYRIGHT 2003 O'DELL ENGINEERING LTD.
+ */
+package ca.odell.glazedlists.net;
+
+// NIO is used for CTP
+import java.util.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.net.*;
+import java.io.*;
+// logging
+import java.util.logging.*;
+
+/**
+ * A CTPRunnable that unblocks the calling thread when it finishes executing.
+ */
+class CTPBlockingRunnable implements CTPRunnable {
+    
+    /** the target runnable */
+    private CTPRunnable target;
+    
+    /**
+     * Creates a CTPBlockingRunnable that runs the specified target while the calling
+     * thread waits.
+     */
+    public CTPBlockingRunnable(CTPRunnable target) {
+        this.target = target;
+    }
+    
+    /**
+     * Runs the specified task.
+     *
+     * @param selector the selector being shared by all connections.
+     * @return true unless the server shall shutdown due to a shutdown request or
+     *      an unrecoverable failure.
+     */
+    public void run(Selector selector, CTPConnectionManager manager) {
+         target.run(selector, manager);
+         
+         // we're done, wake up the waiting thread
+         synchronized(this) {
+             notify();
+         }
+    }
+}
---
+++ source/ca/odell/glazedlists/net/CTPChunkToSend.java
@@ -0,0 +1,58 @@
+/**
+ * Glazed Lists
+ * http://glazedlists.dev.java.net/
+ *
+ * COPYRIGHT 2003 O'DELL ENGINEERING LTD.
+ */
+package ca.odell.glazedlists.net;
+
+// NIO is used for CTP
+import java.util.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.net.*;
+import java.io.*;
+// logging
+import java.util.logging.*;
+
+/**
+ * Sends a chunk of data on the CTPConnectionManager thread.
+ */
+class CTPChunkToSend implements CTPRunnable {
+     
+    /** logging */
+    private static Logger logger = Logger.getLogger(CTPChunkToSend.class.toString());
+
+    /** the destination */
+    private CTPConnection connection;
+
+    /** the content */
+    private ByteBuffer data;
+
+    /**
+     * Create a new CTPConnectionToEstablish.
+     */
+    public CTPChunkToSend(CTPConnection connection, ByteBuffer data) {
+        this.connection = connection;
+        this.data = data;
+    }
+    
+    /**
+     * Writes the data.
+     */
+    public void run(Selector selector, CTPConnectionManager manager) {
+        if(connection.state != CTPConnection.STATE_READY) throw new IllegalStateException();
+        
+        try {
+            String chunkSizeInHex = Integer.toString(data.remaining(), 16);
+            connection.writer.write(chunkSizeInHex);
+            connection.writer.write("\r\n");
+            connection.writer.write(data);
+            connection.writer.write("\r\n");
+            connection.writer.requestFlush();
+            
+        } catch(IOException e) {
+            connection.close(e);
+        }
+    }
+}
