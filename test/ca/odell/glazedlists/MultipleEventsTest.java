35f1a9d4426046f2c64672d6b0dd2b857b3d69b5
Jesse Wilson <jesse@swank.ca>
2004-11-22 20:45:23 +0000

    pulled out a class into the main package Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@454 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/MultipleEventsTest.java
+++ test/ca/odell/glazedlists/MultipleEventsTest.java
@@ -115,38 +115,3 @@ public class MultipleEventsTest extends TestCase {
         assertEquals(1, counter.getEventCount());
     }
 }
-
-
-/**
- * Counts how many ListEvents are received.
- */
-class ListEventCounter implements ListEventListener {
-    
-    /** count the number of changes per event */
-    private List changeCounts = new ArrayList();
-
-    /**
-     * When an event occurs, count that.
-     */
-    public void listChanged(ListEvent listChanges) {
-        int changesForEvent = 0;
-        while(listChanges.next()) {
-            changesForEvent++;
-        }
-        changeCounts.add(new Integer(changesForEvent));
-    }
-
-    /**
-     * Gets the number of events that have occured thus far.
-     */
-    public int getEventCount() {
-        return changeCounts.size();
-    }
-
-    /**
-     * Gets the number of changes for the specified event.
-     */
-    public int getChangeCount(int event) {
-        return ((Integer)changeCounts.get(event)).intValue();
-    }
-}
