c6815d24fc0f9bd2026b88c6481ccda8da924fe6
Jesse Wilson <jesse@swank.ca>
2004-11-22 20:42:19 +0000

    pulled counter out
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@453 696c4f5c-992c-4387-df9e-f6c076136008

---
+++ test/ca/odell/glazedlists/ListEventCounter.java
@@ -0,0 +1,52 @@
+/**
+ * Glazed Lists
+ * http://glazedlists.dev.java.net/
+ *
+ * COPYRIGHT 2003 O'DELL ENGINEERING LTD.
+ */
+package ca.odell.glazedlists;
+
+// for being a JUnit test case
+import junit.framework.*;
+// the core Glazed Lists package
+import ca.odell.glazedlists.util.*;
+// the Glazed Lists' change objects
+import ca.odell.glazedlists.event.*;
+// Java collections are used for underlying data storage
+import java.util.*;
+
+/**
+ * Counts how many ListEvents are received.
+ *
+ * @author <a href="mailto:jesse@odel.on.ca">Jesse Wilson</a>
+ */
+class ListEventCounter implements ListEventListener {
+    
+    /** count the number of changes per event */
+    private List changeCounts = new ArrayList();
+
+    /**
+     * When an event occurs, count that.
+     */
+    public void listChanged(ListEvent listChanges) {
+        int changesForEvent = 0;
+        while(listChanges.next()) {
+            changesForEvent++;
+        }
+        changeCounts.add(new Integer(changesForEvent));
+    }
+
+    /**
+     * Gets the number of events that have occured thus far.
+     */
+    public int getEventCount() {
+        return changeCounts.size();
+    }
+
+    /**
+     * Gets the number of changes for the specified event.
+     */
+    public int getChangeCount(int event) {
+        return ((Integer)changeCounts.get(event)).intValue();
+    }
+}
