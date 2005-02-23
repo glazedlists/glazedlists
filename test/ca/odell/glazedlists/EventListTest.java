edf7c3acd25b2b3c22b72b9d64848813786fe601
Jesse Wilson <jesse@swank.ca>
2005-02-23 23:06:25 +0000

    new test cases for bug 189
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@732 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/EventListTest.java
+++ test/ca/odell/glazedlists/EventListTest.java
@@ -59,7 +59,6 @@ public class EventListTest extends TestCase {
         assertEquals(jesseArrayList, jesseSortedList);
     }
 
-
     /**
      * Validates that retainAll() works.
      */
@@ -83,5 +82,249 @@ public class EventListTest extends TestCase {
         jesseSortedList.addAll(jesse);
         jesseSortedList.retainAll(wilson);
         assertEquals(jesseArrayList, jesseSortedList);
+    }
+
+    /**
+     * Validates that contains() works with null.
+     */
+    public void testContainsNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            assertEquals(false, list.contains(null));
+            assertEquals(true,  list.contains("Western"));
+            
+            // test a list that does contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            assertEquals(true, list.contains(null));
+            assertEquals(true, list.contains("Western"));
+            assertEquals(false, list.contains("Molson"));
+        }
+    }
+
+    /**
+     * Validates that containsAll() works with null.
+     */
+    public void testContainsAllNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            assertEquals(true, list.containsAll(Arrays.asList(new String[] { "Sleeman", "Molson" })));
+            assertEquals(false, list.containsAll(Arrays.asList(new String[] { "Molson", null })));
+            assertEquals(false, list.containsAll(Arrays.asList(new String[] { "Molson", "Busch" })));
+            
+            // test a list that does contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            assertEquals(false, list.containsAll(Arrays.asList(new String[] { "Sleeman", "Molson" })));
+            assertEquals(true, list.containsAll(Arrays.asList(new String[] { "Sleeman", "Western" })));
+            assertEquals(true, list.containsAll(Arrays.asList(new String[] { "Western", null })));
+            assertEquals(true, list.containsAll(Arrays.asList(new String[] { null, null })));
+        }
+    }
+
+    /**
+     * Validates that indexOf() works with null.
+     */
+    public void testIndexOfNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            assertTrue(-1 == list.indexOf(null));
+            assertTrue(-1 != list.indexOf("Western"));
+            
+            // test a list that does contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            assertTrue(-1 != list.indexOf(null));
+            assertTrue(-1 != list.indexOf("Western"));
+            assertTrue(-1 == list.indexOf("Molson"));
+        }
+    }
+
+
+
+    /**
+     * Validates that lastIndexOf() works with null.
+     */
+    public void testLastIndexOfNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            assertTrue(-1 == list.lastIndexOf(null));
+            assertTrue(-1 != list.lastIndexOf("Western"));
+            
+            // test a list that does contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            assertTrue(-1 != list.lastIndexOf(null));
+            assertTrue(-1 != list.lastIndexOf("Western"));
+            assertTrue(-1 == list.lastIndexOf("Molson"));
+        }
+    }
+
+    /**
+     * Validates that remove() works with null.
+     */
+    public void testRemoveNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            assertEquals(false, list.remove(null));
+            assertEquals(true,  list.remove("Sleeman"));
+            
+            // test a list that does contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            assertEquals(true, list.remove(null));
+            assertEquals(true, list.remove("Western"));
+            assertEquals(false, list.remove("Molson"));
+        }
+    }
+
+    /**
+     * Validates that removeAll() works with null.
+     */
+    public void testRemoveAllNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            assertEquals(true, list.removeAll(Arrays.asList(new String[] { "Western", null })));
+            assertEquals(false,  list.removeAll(Arrays.asList(new String[] { null, "Busch" })));
+            
+            // test a list that does contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            assertEquals(true, list.removeAll(Arrays.asList(new String[] { "Western", "Busch" })));
+            assertEquals(true, list.removeAll(Arrays.asList(new String[] { "Sleeman", null })));
+            assertEquals(false, list.removeAll(Arrays.asList(new String[] { "Western", null })));
+        }
+    }
+
+    /**
+     * Validates that retainAll() works with null.
+     */
+    public void testRetainAllNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            assertEquals(true,  list.retainAll(Arrays.asList(new String[] { "Western", null })));
+            assertEquals(true, list.retainAll(Arrays.asList(new String[] { "Moslon", null })));
+            
+            // test a list that does contain nulls
+            list.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            assertEquals(true,  list.retainAll(Arrays.asList(new String[] { "Western", null })));
+            assertEquals(true, list.retainAll(Arrays.asList(new String[] { "Moslon", null })));
+        }
+    }
+
+
+    /**
+     * Validates that hashCode() works with null.
+     */
+    public void testHashCodeNull() {
+        // get all different list types
+        List listTypes = new ArrayList();
+        listTypes.add(new ArrayList());
+        listTypes.add(new BasicEventList());
+        listTypes.add(new SortedList(new BasicEventList()));
+        
+        // test all different list types
+        for(Iterator i = listTypes.iterator(); i.hasNext(); ) {
+            List list = (List)i.next();
+            List copy = new ArrayList();
+            
+            // test a list that doesn't contain nulls
+            list.clear();
+            copy.clear();
+            list.addAll(Arrays.asList(new String[] { "Molson", "Sleeman", "Labatts", "Western" }));
+            copy.addAll(list);
+            assertEquals(copy.hashCode(), list.hashCode());
+            assertTrue(list.equals(copy));
+            copy.set(0, "Busch");
+            assertFalse(list.equals(copy));
+            
+            // test a list that does contain nulls
+            list.clear();
+            copy.clear();
+            list.addAll(Arrays.asList(new String[] { null, "Sleeman", null, "Western" }));
+            copy.addAll(list);
+            assertEquals(copy.hashCode(), list.hashCode());
+            assertTrue(list.equals(copy));
+            copy.set(0, "Busch");
+            assertFalse(list.equals(copy));
+        }
     }
 }
