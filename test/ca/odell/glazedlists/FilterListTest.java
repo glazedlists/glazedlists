e3b1e3f5c5d546cd6e6ce74715152f438364ed28
Jesse Wilson <jesse@swank.ca>
2005-04-04 22:41:45 +0000

    Now with a new test that fails Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@820 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/FilterListTest.java
+++ test/ca/odell/glazedlists/FilterListTest.java
@@ -18,23 +18,88 @@ import java.util.*;
 public class FilterListTest extends TestCase {
 
     /**
-     * This test demonstrates a Issue 213.
+     * This test demonstrates Issue 213.
      */
     public void testRelax() {
         // construct a (contrived) list of initial values
-        int[] values = new int [] { 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 0, 10, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 1 };
         BasicEventList original = new BasicEventList();
-        for(int i = 0; i < values.length; i++) {
-            original.add(new Integer(values[i]));
-        }
+        List values = intArrayToIntegerCollection(new int [] { 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 0, 10, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 1 });
+        original.addAll(values);
         
         // prepare a filter to filter our list
         MinimumValueMatcherEditor editor = new MinimumValueMatcherEditor();
         FilterList myFilterList = new FilterList(original, editor);
+        myFilterList.addListEventListener(new ConsistencyTestList(myFilterList, "filter"));
         
         // relax the list
         editor.setMinimum(11);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
         editor.setMinimum(10);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(0);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(10);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(11);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(0);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        
+        // now try constrain
+        values = intArrayToIntegerCollection(new int[] { 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11 });
+        original.clear();
+        original.addAll(values);
+        
+        // constrain the list
+        editor.setMinimum(10);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(11);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(12);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(10);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+
+        // now try more changes
+        values = intArrayToIntegerCollection(new int[] { 8, 6, 7, 5, 3, 0, 9 });
+        original.clear();
+        original.addAll(values);
+
+        // constrain the list
+        editor.setMinimum(5);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(10);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(1);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(0);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+        editor.setMinimum(1);
+        assertEquals(myFilterList, filter(original, editor.getMatcher()));
+
+    }
+
+    /**
+     * Convert the specified int[] array to a List of Integers.
+     */
+    private List intArrayToIntegerCollection(int[] values) {
+        List result = new ArrayList();
+        for(int i = 0; i < values.length; i++) {
+            result.add(new Integer(values[i]));
+        }
+        return result;
+    }
+    
+    /**
+     * Manually apply the specified filter to the specified list.
+     */
+    private List filter(List input, Matcher matcher) {
+        List result = new ArrayList();
+        for(Iterator i = input.iterator(); i.hasNext(); ) {
+            Object element = i.next();
+            if(matcher.matches(element)) result.add(element);
+        }
+        return result;
     }
     
     /**
