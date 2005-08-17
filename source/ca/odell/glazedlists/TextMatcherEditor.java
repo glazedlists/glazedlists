6539a9428b3378a4842bebc52756745ec48289c1
Jesse Wilson <jesse@swank.ca>
2005-08-17 04:16:21 +0000

    Pulled out AbstractValueMatcherEditor from TextMatcherEditor. The problem with this code was that the superclass was doing weird stuff in its constructor that shouldn't have been called until construction was complete of the subclass.
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@983 696c4f5c-992c-4387-df9e-f6c076136008

--- source/ca/odell/glazedlists/TextMatcherEditor.java
+++ source/ca/odell/glazedlists/TextMatcherEditor.java
@@ -1,5 +1,5 @@
 /* Glazed Lists                                                 (c) 2003-2005 */
-/* http://publicobject.com/glazedlists/                      publicobject.com,*/
+/* <a href="http://publicobject.com/glazedlists/">http://publicobject.com/glazedlists/</a>                      publicobject.com,*/
 /*                                                     O'Dell Engineering Ltd.*/
 package ca.odell.glazedlists;
 
@@ -13,15 +13,15 @@ import ca.odell.glazedlists.impl.filter.*;
  * subclasses. This matcher is fully concrete, and may be used directly by
  * headless applications.
  *
- * <p>The {@link TextMatcherEditor} requires that either a
+ * &lt;p&gt;The {@link TextMatcherEditor} requires that either a
  * {@link TextFilterator} be specified in its constructor, or that every Object
  * matched implements the {@link TextFilterable} interface. These
  * are used to specify the {@link String}s to search for each Object.
  *
  * @author James Lemieux
- * @author <a href="mailto:jesse@odel.on.ca">Jesse Wilson</a>
+ * @author &lt;a href="mailto:<a href="mailto:jesse@odel.on.ca">jesse@odel.on.ca</a>"&gt;Jesse Wilson&lt;/a&gt;
  */
-public class TextMatcherEditor extends AbstractValueMatcherEditor {
+public class TextMatcherEditor extends AbstractMatcherEditor {
 
     /** the filterator is used as an alternative to implementing the TextFilterable interface */
     private final TextFilterator filterator;
@@ -33,7 +33,7 @@ public class TextMatcherEditor extends AbstractValueMatcherEditor {
      * Creates a {@link TextMatcherEditor} whose Matchers can test only elements which
      * implement the {@link TextFilterable} interface.
      *
-     * <p>The {@link Matcher}s from this {@link MatcherEditor} will fire a
+     * &lt;p&gt;The {@link Matcher}s from this {@link MatcherEditor} will fire a
      * {@link ClassCastException} when called with an Object that does not implement
      * {@link TextFilterable}.
      */
@@ -42,18 +42,22 @@ public class TextMatcherEditor extends AbstractValueMatcherEditor {
     }
 
     /**
+     * Get the filterator used to extract Strings from the matched elements.
+     */
+    public TextFilterator getFilterator() {
+        return filterator;
+    }
+
+    /**
      * Creates a {@link TextMatcherEditor} that matches Objects using the
      * specified {@link TextFilterator} to get the {@link String}s to search.
      *
      * @param filterator the object that will extract filter Strings from each
-     *      object in the <code>source</code>; <code>null</code> indicates the
+     *      object in the &lt;code&gt;source&lt;/code&gt;; &lt;code&gt;null&lt;/code&gt; indicates the
      *      list elements implement {@link TextFilterable}
      */
     public TextMatcherEditor(TextFilterator filterator) {
-		// Start with normal logic and no value set
-		super( false, null );
-
-		this.filterator = filterator;
+        this.filterator = filterator;
     }
 
     /**
@@ -63,55 +67,22 @@ public class TextMatcherEditor extends AbstractValueMatcherEditor {
      * @param filterStrings the {@link String}s representing all of the filter values
      */
     public void setFilterText(String[] filterStrings) {
-		if (filterStrings == null) filterStrings = new String[0];
-
-		String[] oldFilters = this.filters;
+        String[] oldFilters = this.filters;
         this.filters = TextMatcher.normalizeFilters(filterStrings);
 
-		boolean need_to_fire_update = setValue(TextMatcher.normalizeFilters(filterStrings));
-		if (need_to_fire_update) {
+        // fire the event only as necessary
+        if(!TextMatcher.isFilterEqual(oldFilters, filters)) {
 
-			// fire the event only as necessary
-			if(!TextMatcher.isFilterEqual(oldFilters, filters)) {
-
-				// classify the change in filter and apply the new filter to this list
-				if(filters.length == 0) {
-					fireMatchAll();
-				} else if(TextMatcher.isFilterRelaxed(oldFilters, filters)) {
-					fireRelaxed(getMatcher());
-				} else if(TextMatcher.isFilterConstrained(oldFilters, filters)) {
-					fireConstrained(getMatcher());
-				} else {
-					fireChanged(getMatcher());
-				}
-			}
-		}
+            // classify the change in filter and apply the new filter to this list
+            if(filters.length == 0) {
+                fireMatchAll();
+            } else if(TextMatcher.isFilterRelaxed(oldFilters, filters)) {
+                fireRelaxed(new TextMatcher(filters, filterator));
+            } else if(TextMatcher.isFilterConstrained(oldFilters, filters)) {
+                fireConstrained(new TextMatcher(filters, filterator));
+            } else {
+                fireChanged(new TextMatcher(filters, filterator));
+            }
+        }
     }
-
-
-	/**
-	 * Returns the text filterator used by this editor.
-	 * <p/>
-	 * Note: It's recommended that the filterator be immutable. However, if it is not and
-	 * changes are made to it such that it would return different filter values,
-	 * {@link #fireChanged(Matcher)} should be called to tell the listeners that this
-	 * matcher's value has changed.
-	 */
-	public TextFilterator getTextFilterator() {
-		return filterator;
-	}
-
-
-	/**
-	 * {@inheritDoc}
-	 *
-	 * Create the matcher for the filter string.
-	 *
-	 * Note: this will not
-	 *
-	 * @param value		The filter strings
-	 */
-	protected Matcher createMatcher(Object value) {
-		return new TextMatcher((String[])value, filterator);
-	}
 }
