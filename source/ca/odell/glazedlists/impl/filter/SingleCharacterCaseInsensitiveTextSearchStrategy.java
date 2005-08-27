6c6ccba823b5db818d58067d6ecb8ca9c9d577cf
Jesse Wilson <jesse@swank.ca>
2005-08-27 07:28:12 +0000

    JAMES is in a new competition with K-MALTBY to make Glazed Lists faster. Who will win? The Glazed Lists users, of course!
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@996 696c4f5c-992c-4387-df9e-f6c076136008

--- source/ca/odell/glazedlists/impl/filter/SingleCharacterCaseInsensitiveTextSearchStrategy.java
+++ source/ca/odell/glazedlists/impl/filter/SingleCharacterCaseInsensitiveTextSearchStrategy.java
@@ -19,6 +19,14 @@ public class SingleCharacterCaseInsensitiveTextSearchStrategy implements TextSea
     /** <tt>true</tt> if subtext has been set; <tt>false</tt> otherwise. */
     private boolean subtextInitialized = false;
 
+    /** caching upper case characters has shown a 5% performance boost over Character.toUpperCase() */
+    private static final char[] UPPER_CASE_CACHE = new char[256];
+    static {
+        for(char c = 0; c < UPPER_CASE_CACHE.length; c++) {
+            UPPER_CASE_CACHE[c] = Character.toUpperCase(c);
+        }
+    }
+
     /**
      * Sets the subtext to locate found when {@link #indexOf(String)} is called.
      * This
@@ -30,10 +38,10 @@ public class SingleCharacterCaseInsensitiveTextSearchStrategy implements TextSea
      *      character
      */
     public void setSubtext(String subtext) {
-        if(subtext == null) throw new IllegalArgumentException("subtext may not be null");
+        if (subtext == null) throw new IllegalArgumentException("subtext may not be null");
         if (subtext.length() != 1) throw new IllegalArgumentException("subtext (" + subtext + ") must contain a single character");
 
-        this.subtextChar = Character.toUpperCase(subtext.charAt(0));
+        this.subtextChar = toUpperCase(subtext.charAt(0));
         this.subtextInitialized = true;
     }
 
@@ -42,17 +50,25 @@ public class SingleCharacterCaseInsensitiveTextSearchStrategy implements TextSea
         // ensure we are in a state to search the text
         if(!this.subtextInitialized) throw new IllegalStateException("setSubtext must be called with a valid value before this method can operate");
 
-        // extract the length of text one time
-        final int textLength = text.length();
-
         // search for subtextChar in the given text
-        for(int c = 0; c < textLength; c++) {
-            if(Character.toUpperCase(text.charAt(c)) == this.subtextChar) {
+        for(int c = 0; c < text.length(); c++) {
+            if(toUpperCase(text.charAt(c)) == this.subtextChar) {
                 return c;
             }
         }
 
         // we didn't find the subtextChar so return -1
         return -1;
+    }
+
+    /**
+     * Our superfast toUpperCase method only works when a character is in the base
+     * 256 characters. Those values are precalculated because Character.toUpperCase()
+     * has proven to be a bottleneck.
+     *
+     * @see <a href="https://glazedlists.dev.java.net/issues/show_bug.cgi?id=136">Issue 136</a>
+     */
+    private static char toUpperCase(char anyCase) {
+        return anyCase < UPPER_CASE_CACHE.length ? UPPER_CASE_CACHE[anyCase] : Character.toUpperCase(anyCase);
     }
 }
