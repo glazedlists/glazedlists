2993bc00d3cce38f2a3e4564e4ae6d32f763ec8d
James Lemieux <jplemieux@gmail.com>
2005-11-11 08:51:47 +0000

    Testing of GroupList
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1070 696c4f5c-992c-4387-df9e-f6c076136008

--- source/ca/odell/glazedlists/GroupingList.java
+++ source/ca/odell/glazedlists/GroupingList.java
@@ -28,7 +28,7 @@ import java.util.*;
  * <tr><td class="tablesubheadingcolor"><b>Concurrency:</b></td><td>thread ready, not thread safe</td></tr>
  * <tr><td class="tablesubheadingcolor"><b>Performance:</b></td><td>reads: O(log N), writes O(log N)</td></tr>
  * <tr><td class="tablesubheadingcolor"><b>Memory:</b></td><td></td></tr>
- * <tr><td class="tablesubheadingcolor"><b>Unit Tests:</b></td><td>N/A</td></tr>
+ * <tr><td class="tablesubheadingcolor"><b>Unit Tests:</b></td><td>GroupingListTest</td></tr>
  * <tr><td class="tablesubheadingcolor"><b>Issues:</b></td><td>N/A</td></tr>
  * </table>
  */
