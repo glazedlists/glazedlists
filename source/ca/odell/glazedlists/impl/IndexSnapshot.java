ba3828cdaa1f733cccafbd341be2803866df8f27
James Lemieux <jplemieux@gmail.com>
2005-11-19 09:19:17 +0000

    no message
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1077 696c4f5c-992c-4387-df9e-f6c076136008

--- source/ca/odell/glazedlists/impl/IndexSnapshot.java
+++ source/ca/odell/glazedlists/impl/IndexSnapshot.java
@@ -18,14 +18,14 @@ import ca.odell.glazedlists.impl.adt.Barcode;
  * <p>For example, suppose you have the sequence:
  * <code>F, I, L, T, E, R</code>, and you have performed some time-intensive
  * calculation on those elements (such as creating a sorted view of it). In the
- * initial case, the {@link ca.odell.glazedlists.IndexSnapshot} provides a natural function to convert
+ * initial case, the {@link IndexSnapshot} provides a natural function to convert
  * between indices:
  * <code>currentToSnapshot(0) -> 0</code>,
  * <code>currentToSnapshot(5) -> 5</code>.
  *
  * <p>When you change that sequence by removing the 'I' and 'E', the
- * {@link ca.odell.glazedlists.IndexSnapshot} we have the sequence <code>F, L, T, R</code>.
- * The {@link ca.odell.glazedlists.IndexSnapshot} provides a more value in this case:
+ * {@link IndexSnapshot} we have the sequence <code>F, L, T, R</code>.
+ * The {@link IndexSnapshot} provides more value in this case:
  * <code>currentToSnapshot(0) -> 0</code>,
  * <code>currentToSnapshot(3) -> 5</code>,
  * <code>snapshotToCurrent(0) -> 0</code>,
@@ -34,7 +34,7 @@ import ca.odell.glazedlists.impl.adt.Barcode;
  *
  * <p>Similarly you can change the sequence again by adding elements
  * <code>O, A</code> to create the sequence <code>F, L, O, A, T, R</code>.
- * The {@link ca.odell.glazedlists.IndexSnapshot} provides these results:
+ * The {@link IndexSnapshot} provides these results:
  * <code>currentToSnapshot(1) -> 2</code>,
  * <code>currentToSnapshot(2) -> 7</code>,
  * <code>currentToSnapshot(3) -> 8</code>,
@@ -61,7 +61,6 @@ class IndexSnapshot {
      * always equal to the length of the current view.
      */
     private final Barcode inserts = new Barcode();
-
 
     public void reset(int length) {
         deletes.clear();
@@ -127,16 +126,14 @@ class IndexSnapshot {
     public void remove(int currentIndex) {
         if(currentIndex < 0 || currentIndex >= currentSize()) throw new IndexOutOfBoundsException();
 
-        // if this was a snapshot element, we need to mark
-        // this as deleted from the deletes list
+        // if this was a snapshot element, we need to mark this as deleted from the deletes list
         if(inserts.get(currentIndex) == Barcode.BLACK) {
             int insertAdjusted = inserts.getBlackIndex(currentIndex);
             int deleteAdjusted = deletes.getIndex(insertAdjusted, Barcode.BLACK);
             deletes.set(deleteAdjusted, Barcode.WHITE, 1);
             inserts.remove(currentIndex, 1);
 
-        // if this was not a snapshot element, we can just
-        // de-insert it
+        // if this was not a snapshot element, we can just de-insert it
         } else {
             inserts.remove(currentIndex, 1);
         }
