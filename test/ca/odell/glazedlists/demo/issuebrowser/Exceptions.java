4312d925691771e1d81396cb58163bfcd8b1fc2e
James Lemieux <jplemieux@gmail.com>
2005-12-13 07:31:37 +0000

    Created a new Exceptions repository for handling application wide exceptions like UnknownHostExceptions. (The Exceptions repository is necessary to separate the strategies for notifying the user in Swing and SWT.)
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1123 696c4f5c-992c-4387-df9e-f6c076136008

---
+++ test/ca/odell/glazedlists/demo/issuebrowser/Exceptions.java
@@ -0,0 +1,83 @@
+package ca.odell.glazedlists.demo.issuebrowser;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * This class contains a repository of strategies for dealing with Exceptions
+ * which occur any where in the application. Clients may register Exception
+ * handlers to deal with these exception in any way they see fit. If no
+ * registered Exception handler recognizes an exception that has been raised
+ * it is ignored by default.
+ *
+ * @author James Lemieux
+ */
+public final class Exceptions {
+
+    /**
+     * The interface Exception handlers must implement. It has two methods to
+     * containing the logic for each of its concerns: recognizing Exceptions
+     * it can handle and handling those Exceptions appropriately.
+     */
+    public interface Handler {
+        /**
+         * Return <tt>true</tt> if this Handler can work with the given
+         * Exception; <tt>false</tt> otherwise.
+         */
+        public boolean recognize(Exception e);
+
+        /**
+         * React to the given Exception in any way seen fit. This may include
+         * notifying the user, writing to a log, or any other valid logic.
+         */
+        public void handle(Exception e);
+    }
+
+    /** The one instance of the Exceptions class that is allowed. */
+    private static final Exceptions singleton = new Exceptions();
+
+    /**
+     * Returns a handle to the single instance of the Exceptions class which is
+     * allowed to exist.
+     */
+    public static Exceptions getInstance() {
+        return singleton;
+    }
+
+    /**
+     * A list of handlers which handle Exceptions of a global nature.
+     */
+    private final List<Handler> handlers = new ArrayList<Handler>();
+
+    /**
+     * Add <code>h</code> to the collection of {@link Handler}s consulted when
+     * an Exception is raised from within the application.
+     */
+    public void addHandler(Handler h) {
+        handlers.add(h);
+    }
+
+    /**
+     * Remove <code>h</code> from the collection of {@link Handler}s consulted when
+     * an Exception is raised from within the application.
+     */
+    public void removeHandler(Handler h) {
+        handlers.remove(h);
+    }
+
+    /**
+     * Attempt to locate a {@link Handler} which
+     * {@link Handler#recognize recognizes} the given Exception and give it a
+     * chance to {@link Handler#handle handle} it.
+     */
+    public void handle(Exception e) {
+        for (Iterator<Handler> i = handlers.iterator(); i.hasNext();) {
+            Handler handler = i.next();
+            if (handler.recognize(e)) {
+                handler.handle(e);
+                return;
+            }
+        }
+    }
+}
