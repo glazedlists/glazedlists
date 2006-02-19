23a6ba6335b1c8b1ad3d02b816cd6476096c5cd7
James Lemieux <jplemieux@gmail.com>
2006-02-19 08:58:34 +0000

    Open Issues Over Time chart now has well formatted time axis.
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1261 696c4f5c-992c-4387-df9e-f6c076136008

--- extensions/issuesbrowser/source/com/publicobject/issuesbrowser/swing/IssueSummaryChartsComponent.java
+++ extensions/issuesbrowser/source/com/publicobject/issuesbrowser/swing/IssueSummaryChartsComponent.java
@@ -14,13 +14,19 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.general.PieDataset;
+import org.jfree.text.TextBlock;
+import org.jfree.ui.RectangleEdge;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * This component is placed below the issues table and is shown when no issues
@@ -52,7 +58,7 @@ class IssueSummaryChartsComponent {
 
         // build a Line Chart and a panel to display it
         final JFreeChart lineChart_OpenIssuesOverTime = ChartFactory.createLineChart("Open Issues Over Time", "Time", "Open Issues", new OpenIssuesByMonthCategoryDataset(issuesList), PlotOrientation.VERTICAL, true, true, false);
-        lineChart_OpenIssuesOverTime.getCategoryPlot().getDomainAxis().setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+        lineChart_OpenIssuesOverTime.getCategoryPlot().setDomainAxis(new CustomCategoryAxis());
         this.lineChartPanel_OpenIssuesOverTime = new ChartPanel(lineChart_OpenIssuesOverTime, true);
 
         // add all ChartPanels to a master panel
@@ -74,6 +80,24 @@ class IssueSummaryChartsComponent {
     private static class StatusFunction implements FunctionList.Function<List,Comparable> {
         public Comparable evaluate(List sourceValue) {
             return ((Issue) sourceValue.get(0)).getStatus();
+        }
+    }
+
+    /**
+     * This custom CategoryAxis formats the labels of the Date axis as desired.
+     */
+    private static class CustomCategoryAxis extends CategoryAxis {
+        private static final DateFormat DATE_FORMAT = new SimpleDateFormat("MMMMM yyyy");
+
+        public CustomCategoryAxis() {
+            // display the labels vertically rather than horizontally
+            this.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+        }
+
+        protected TextBlock createLabel(Comparable category, float width, RectangleEdge edge, Graphics2D g2) {
+            // formate the date string before creating the label
+            final String date = DATE_FORMAT.format((Date) category);
+            return super.createLabel(date, width, edge, g2);
         }
     }
 }
