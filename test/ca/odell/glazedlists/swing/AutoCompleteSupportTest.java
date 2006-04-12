3852b72670b87098180bf2a891fbe860d5d6ef42
James Lemieux <jplemieux@gmail.com>
2006-04-12 07:43:10 +0000

    corrected the testcase given the new ActionEvent firing behaviour
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1394 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/swing/AutoCompleteSupportTest.java
+++ test/ca/odell/glazedlists/swing/AutoCompleteSupportTest.java
@@ -193,57 +193,57 @@ public class AutoCompleteSupportTest extends SwingTestCase {
         doc.replace(0, 0, "New", null);
         assertEquals(2, combo.getItemCount());
         assertEquals("New Brunswick", textField.getText());
-        assertEquals(0, listener.getCount());
+        assertEquals(1, listener.getCount());
 
         // typing over all the text in a Document
         doc.replace(0, doc.getLength(), "x", null);
         assertEquals(0, combo.getItemCount());
         assertEquals("x", textField.getText());
-        assertEquals(0, listener.getCount());
+        assertEquals(2, listener.getCount());
 
         // appending to a Document
         doc.insertString(1, "y", null);
         assertEquals(0, combo.getItemCount());
         assertEquals("xy", textField.getText());
-        assertEquals(0, listener.getCount());
+        assertEquals(2, listener.getCount());
 
         // removing from a Document
         doc.remove(1, 1);
         assertEquals(0, combo.getItemCount());
         assertEquals("x", textField.getText());
-        assertEquals(0, listener.getCount());
+        assertEquals(2, listener.getCount());
 
         // removing the last char from a Document
         doc.remove(0, 1);
         assertEquals(items.size(), combo.getItemCount());
         assertEquals("", textField.getText());
-        assertEquals(0, listener.getCount());
+        assertEquals(2, listener.getCount());
 
         // setting in text through the JTextField
         textField.setText("Prince");
         assertEquals(1, combo.getItemCount());
         assertEquals("Prince Edward Island", textField.getText());
         assertEquals(" Edward Island", textField.getSelectedText());
-        assertEquals(0, listener.getCount());
+        assertEquals(3, listener.getCount());
 
         // simulate the enter key
         textField.postActionEvent();
         assertEquals(1, combo.getItemCount());
         assertEquals("Prince Edward Island", textField.getText());
         assertEquals(null, textField.getSelectedText());
-        assertEquals(1, listener.getCount());
+        assertEquals(4, listener.getCount());
 
         // select an index programmatically
         combo.setSelectedIndex(-1);
         assertEquals(1, combo.getItemCount());
         assertEquals("", textField.getText());
-        assertEquals(2, listener.getCount());
+        assertEquals(5, listener.getCount());
 
         // select a value programmatically
         combo.setSelectedItem("Prince Edward Island");
         assertEquals(1, combo.getItemCount());
         assertEquals("Prince Edward Island", textField.getText());
-        assertEquals(3, listener.getCount());
+        assertEquals(6, listener.getCount());
     }
 
 
