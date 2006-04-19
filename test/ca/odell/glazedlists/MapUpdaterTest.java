bf302f758a2debdc18a76b891711f91a13769c02
Jesse Wilson <jesse@swank.ca>
2006-04-19 00:48:57 +0000

    added andy's test for MapUpdater
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1428 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/MapUpdaterTest.java
+++ test/ca/odell/glazedlists/MapUpdaterTest.java
@@ -6,6 +6,9 @@ package ca.odell.glazedlists;
 import junit.framework.TestCase;
 
 import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author <a href="mailto:jesse@swank.ca">Jesse Wilson</a>
@@ -44,6 +47,65 @@ public class MapUpdaterTest extends TestCase {
         public String evaluate(String sourceValue) {
             if(sourceValue == null || sourceValue.length() == 0) return "";
             return sourceValue.substring(0, 1);
+        }
+    }
+
+
+    public void testClearSrcListWithGrouping() {
+        final EventList<String> words = new BasicEventList<String>();
+        final GroupingList<String> wordGroups = new GroupingList<String>(words, new FunctionComparator<String, String>(new FirstLetterFunction()));
+        Map<String, List<String>> wordIndex = MapUpdater.mapForEventList(
+                wordGroups,
+                new FunctionChain<List<String>, String, String>(new FirstElementFunction<String>(), new FirstLetterFunction()),
+                MapUpdater.IDENTITY_FUNCTION
+        );
+
+        assertEquals(0, wordIndex.size());
+        words.add("Jodie");
+        words.add("Ashford");
+        words.add("Wilson");
+        assertEquals(3, wordIndex.size());
+        assertEquals(Arrays.asList(new String[] { "Jodie" }), wordIndex.get("J"));
+        assertEquals(Arrays.asList(new String[] { "Ashford" }), wordIndex.get("A"));
+        assertEquals(Arrays.asList(new String[] { "Wilson" }), wordIndex.get("W"));
+        words.clear();
+    }
+
+
+    public static class FirstElementFunction<T> implements FunctionList.Function<List<T>, T> {
+        public T evaluate(final List<T> sourceValue) {
+            return sourceValue != null && sourceValue.size() > 0 ? sourceValue.get(0) : null;
+        }
+    }
+
+    /**
+     * Chain functions.
+     */
+    public static class FunctionChain<A,B,C> implements FunctionList.Function<A, C> {
+        private final FunctionList.Function<A, B> aToB;
+        private final FunctionList.Function<B, C> bToC;
+
+        public FunctionChain(final FunctionList.Function<A, B> aToB, final FunctionList.Function<B, C> bToC) {
+            this.aToB = aToB;
+            this.bToC = bToC;
+        }
+
+        public C evaluate(final A sourceValue) {
+            return bToC.evaluate(aToB.evaluate(sourceValue));
+        }
+    }
+
+    public static class FunctionComparator<A,B extends Comparable<B>> implements Comparator<A> {
+        private final FunctionList.Function<A, B> function;
+
+        public FunctionComparator(final FunctionList.Function<A, B> function) {
+            this.function = function;
+        }
+
+        public int compare(final A a1, final A a2) {
+            final B bc1 = function.evaluate(a1);
+            final B bc2 = function.evaluate(a2);
+            return bc1.compareTo(bc2);
         }
     }
 }
