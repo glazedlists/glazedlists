c2022b3aecdef6e4141204e6168827f72f9299d1
James Lemieux <jplemieux@gmail.com>
2006-08-18 04:15:35 +0000

    optimized imports
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1664 696c4f5c-992c-4387-df9e-f6c076136008

--- extensions/hibernate/source/ca/odell/glazedlists/hibernate/EventListType.java
+++ extensions/hibernate/source/ca/odell/glazedlists/hibernate/EventListType.java
@@ -3,68 +3,53 @@
 /*                                                     O'Dell Engineering Ltd.*/
 package ca.odell.glazedlists.hibernate;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import org.hibernate.EntityMode;
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
 import org.hibernate.HibernateException;
 import org.hibernate.collection.PersistentCollection;
 import org.hibernate.engine.SessionImplementor;
 import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.usertype.UserCollectionType;
 
-import ca.odell.glazedlists.BasicEventList;
-import ca.odell.glazedlists.EventList;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
- * A Hibernate custom collection type for mapping and persisting a {@link BasicEventList} with
- * the help of a {@link PersistentEventList}.
+ * A Hibernate custom collection type for mapping and persisting a
+ * {@link BasicEventList} with the help of a {@link PersistentEventList}.
  * 
  * @author Bruce Alspaugh
  * @author Holger Brands
  */
 public class EventListType implements UserCollectionType {
     
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public boolean contains(Object collection, Object entity) {
         return ((EventList) collection).contains(entity);
     }
     
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public Iterator getElementsIterator(Object collection) {
         return ((EventList) collection).iterator();
     }
     
-    /**
-     * {@inheritDoc}
-     */    
+    /** {@inheritDoc} */
     public Object indexOf(Object collection, Object obj) {
         return new Integer(((EventList) collection).indexOf(obj));
     }
     
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public Object instantiate() {
         return new BasicEventList();
     }
     
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public PersistentCollection instantiate(SessionImplementor session,
             CollectionPersister persister) throws HibernateException {
         return new PersistentEventList(session);
     }
     
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public Object replaceElements(Object original, Object target, CollectionPersister persister,
             Object owner, Map copyCache, SessionImplementor session) throws HibernateException {
         final EventList result = (EventList) target;
@@ -74,11 +59,8 @@ public class EventListType implements UserCollectionType {
         return result;
     }
     
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public PersistentCollection wrap(SessionImplementor session, Object collection) {
         return new PersistentEventList(session, (EventList) collection);
     }
-    
 }
--- extensions/hibernate/source/ca/odell/glazedlists/hibernate/PersistentEventList.java
+++ extensions/hibernate/source/ca/odell/glazedlists/hibernate/PersistentEventList.java
@@ -3,29 +3,25 @@
 /*                                                     O'Dell Engineering Ltd.*/
 package ca.odell.glazedlists.hibernate;
 
-import java.util.List;
-
-import org.hibernate.collection.PersistentList;
-import org.hibernate.engine.SessionImplementor;
-import org.hibernate.persister.collection.CollectionPersister;
-
 import ca.odell.glazedlists.BasicEventList;
 import ca.odell.glazedlists.EventList;
 import ca.odell.glazedlists.event.ListEventListener;
 import ca.odell.glazedlists.event.ListEventPublisher;
 import ca.odell.glazedlists.util.concurrent.ReadWriteLock;
+import org.hibernate.collection.PersistentList;
+import org.hibernate.engine.SessionImplementor;
+import org.hibernate.persister.collection.CollectionPersister;
 
 /**
- * A Hibernate persistent wrapper for an {@link EventList}. Underlying collection implementation is
- * {@link BasicEventList}.
+ * A Hibernate persistent wrapper for an {@link EventList}. Underlying
+ * collection implementation is {@link BasicEventList}.
  * 
  * @author Bruce Alspaugh
  * @author Holger Brands
  */
 public final class PersistentEventList extends PersistentList implements EventList {
 
-    /** SerialUID. */
-    static final long serialVersionUID = 0L;
+    private static final long serialVersionUID = 0L;
 
     /**
      * Constructor with session.
@@ -34,7 +30,7 @@ public final class PersistentEventList extends PersistentList implements EventLi
      */
     public PersistentEventList(SessionImplementor session) {
         super(session);
-        // instantiate list here to avoid NullPointerExceptions with lazy loading.
+        // instantiate list here to avoid NullPointerExceptions with lazy loading
         list = new BasicEventList();
     }
 
@@ -49,37 +45,27 @@ public final class PersistentEventList extends PersistentList implements EventLi
         if (newList == null) throw new IllegalArgumentException("EventList parameter must not be null");
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public void beforeInitialize(CollectionPersister persister) {
         if (this.list == null) throw new IllegalStateException("'list' member is undefined");
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public void addListEventListener(ListEventListener listChangeListener) {
         ((EventList) list).addListEventListener(listChangeListener);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public ListEventPublisher getPublisher() {
         return ((EventList) list).getPublisher();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public ReadWriteLock getReadWriteLock() {
         return ((EventList) list).getReadWriteLock();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     public void removeListEventListener(ListEventListener listChangeListener) {
         ((EventList) list).removeListEventListener(listChangeListener);
     }
