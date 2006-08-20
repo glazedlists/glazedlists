c1ade436a54efea57d0dc361304c27cceb78ce55
Holger Brands <holger.brands@googlemail.com>
2006-08-20 20:58:32 +0000

    Removing tabs...
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1669 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/hibernate/EventListTypeTest.java
+++ test/ca/odell/glazedlists/hibernate/EventListTypeTest.java
@@ -23,21 +23,21 @@ import java.util.List;
  * @author Holger Brands
  */
 public class EventListTypeTest extends AbstractHibernateTestCase {
-	
+
     /**
      * Constructor with name.
      */
-	public EventListTypeTest(String name) {
-		super(name);
-	}
+    public EventListTypeTest(String name) {
+        super(name);
+    }
 
     /**
      * Tests a lazy loaded, one-to-many, unidirectional association with entity collection. 
      */
-	public void testLazyOneToManyUniDirectionalAssociation() {
+    public void testLazyOneToManyUniDirectionalAssociation() {
         doTestOneToManyUniDirectionalAssociation(true);
     }
-    
+
     /**
      * Tests an eager loaded, one-to-many, unidirectional association with entity collection. 
      */
@@ -49,9 +49,9 @@ public class EventListTypeTest extends AbstractHibernateTestCase {
      * Gets mapping files for this test case. 
      */
     protected String[] getMappings() {
-        return new String[] {"User.hbm.xml"};
+        return new String[] { "User.hbm.xml" };
     }
-    
+
     /**
      * Runs tests for a one-to-many, unidirectional association with entity collection.
      * 
@@ -59,38 +59,38 @@ public class EventListTypeTest extends AbstractHibernateTestCase {
      */
     private void doTestOneToManyUniDirectionalAssociation(boolean lazy) {
         // create user with email addresses and persist
-		User u = createAndPersistUser();
-		
+        User u = createAndPersistUser();
+
         // load user and emails in new session
-		Session s = openSession();
-		Transaction t = s.beginTransaction();
+        Session s = openSession();
+        Transaction t = s.beginTransaction();
 
         // test that emails are saved in DB
         List emails = s.createCriteria(Email.class).list();
         assertEquals(emails.size(), 2);
 
         // load saved user again
-		u = loadUser(s, lazy);
+        u = loadUser(s, lazy);
         assertTrue(u != null);
-        
+
         final GlazedListsTests.ListEventCounter<Email> listener = new GlazedListsTests.ListEventCounter<Email>();
-        
+
         if (lazy) {
             // lazy collection still uninitialized
-            assertFalse(Hibernate.isInitialized(u.getEmailAddresses()));        
+            assertFalse(Hibernate.isInitialized(u.getEmailAddresses()));
             // call EventList methods            
             u.getEmailAddresses().addListEventListener(listener);
             u.getEmailAddresses().removeListEventListener(listener);
             u.getEmailAddresses().addListEventListener(listener);
             u.getEmailAddresses().getReadWriteLock();
             u.getEmailAddresses().getPublisher();
-            
+
             // lazy collection should still be uninitialized
             assertFalse(Hibernate.isInitialized(u.getEmailAddresses()));
             assertEquals(0, listener.getCountAndReset());
-            
+
             // trigger initialization        
-    		assertEquals(2, u.getEmailAddresses().size());
+            assertEquals(2, u.getEmailAddresses().size());
             assertTrue(Hibernate.isInitialized(u.getEmailAddresses()));
             // ATTENTION: Hibernate calls add and set for each list element
             assertEquals(4, listener.getCountAndReset());
@@ -99,32 +99,32 @@ public class EventListTypeTest extends AbstractHibernateTestCase {
             assertTrue(Hibernate.isInitialized(u.getEmailAddresses()));
             u.getEmailAddresses().addListEventListener(listener);
         }
-        
+
         // test manipulating list
         u.getEmailAddresses().remove(1);
         u.getEmailAddresses().add(1, new Email("admin@web.de"));
         u.getEmailAddresses().remove(0);
-        
+
         t.commit();
-        s.close();        
+        s.close();
         assertEquals(3, listener.getCountAndReset());
-        
+
         s = openSession();
         t = s.beginTransaction();
         u = loadUser(s, lazy);
-        
+
         assertEquals(1, u.getEmailAddresses().size());
         assertEquals(new Email("admin@web.de"), u.getEmailAddresses().get(0));
         assertTrue(Hibernate.isInitialized(u.getEmailAddresses()));
         emails = s.createCriteria(Email.class).list();
         assertEquals(1, emails.size());
         assertEquals(new Email("admin@web.de"), emails.get(0));
-        
+
         // delete user
-		s.delete(u);
-		t.commit();
-		s.close();
-        
+        s.delete(u);
+        t.commit();
+        s.close();
+
         // no user should be found
         s = openSession();
         t = s.beginTransaction();
@@ -134,11 +134,11 @@ public class EventListTypeTest extends AbstractHibernateTestCase {
         // no emails should be found
         emails = s.createCriteria(Email.class).list();
         assertEquals(0, emails.size());
-        
+
         t.commit();
-        s.close();        
-	}
-    
+        s.close();
+    }
+
     /**
      * Creates and persists an example user with emial addresses.
      */
@@ -154,7 +154,7 @@ public class EventListTypeTest extends AbstractHibernateTestCase {
         s.close();
         return u;
     }
-    
+
     /**
      * Loads the example user.
      * 
@@ -165,8 +165,8 @@ public class EventListTypeTest extends AbstractHibernateTestCase {
             return (User) s.createCriteria(User.class).uniqueResult();
         } else {
             return (User) s.createCriteria(User.class)
-                .setFetchMode("emailAddresses", FetchMode.JOIN)
-                .uniqueResult();
+            	.setFetchMode("emailAddresses", FetchMode.JOIN)
+            	.uniqueResult();
         }
     }
 }
