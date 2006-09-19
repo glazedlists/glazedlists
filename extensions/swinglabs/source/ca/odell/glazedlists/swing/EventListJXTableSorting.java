94804e51f523e9f140c122ec0817af867d34da5d
Jesse Wilson <jesse@swank.ca>
2006-09-19 07:25:52 +0000

    Now with mutating methods on TreeList....
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1756 696c4f5c-992c-4387-df9e-f6c076136008

--- extensions/swinglabs/source/ca/odell/glazedlists/swing/EventListJXTableSorting.java
+++ extensions/swinglabs/source/ca/odell/glazedlists/swing/EventListJXTableSorting.java
@@ -5,6 +5,7 @@ package ca.odell.glazedlists.swing;
 
 import ca.odell.glazedlists.GlazedLists;
 import ca.odell.glazedlists.SortedList;
+import ca.odell.glazedlists.FilterList;
 import ca.odell.glazedlists.gui.AdvancedTableFormat;
 import ca.odell.glazedlists.gui.TableFormat;
 import ca.odell.glazedlists.impl.sort.TableColumnComparator;
@@ -23,30 +24,39 @@ import java.util.*;
  * <p><table border="1" width="100%" cellpadding="3" cellspacing="0">
  * <tr class="TableHeadingColor"><td colspan="2"><font size="+2"><b>Extension: SwingX</b></font></td></tr>
  * <tr><td  colspan="2">This Glazed Lists <i>extension</i> requires the third party library <b>SwingX</b>.</td></tr>
- * <tr><td class="TableSubHeadingColor"><b>Tested Version:</b></td><td>CVS Head, April 15, 2006</td></tr>
+ * <tr><td class="TableSubHeadingColor"><b>Tested Version:</b></td><td>CVS Head, September 19, 2006</td></tr>
  * <tr><td class="TableSubHeadingColor"><b>Home page:</b></td><td><a href="https://swingx.dev.java.net/">https://swingx.dev.java.net/</a></td></tr>
  * <tr><td class="TableSubHeadingColor"><b>License:</b></td><td><a href="http://www.gnu.org/copyleft/lesser.html">LGPL</a></td></tr>
  * </td></tr>
  * </table>
  *
- * <p>To prepare a {@link JXTable} to be sorted using a {@link ca.odell.glazedlists.SortedList}:
- *     <li>Create a {@link ca.odell.glazedlists.SortedList} and {@link EventTableModel} that depends
- *         on that {@link ca.odell.glazedlists.SortedList}.
- *     <li>Create a {@link JXTable} using the {@link EventTableModel} as its model.
- *     <li>Run the {@link EventListJXTableSorting#install} method to bind the
- *         {@link JXTable}'s headers to the {@link ca.odell.glazedlists.SortedList}'s {@link Comparator}.
+ * <p>To prepare a {@link JXTable} to be sorted using a {@link SortedList}:
+ * <li>Create a {@link SortedList} and {@link EventTableModel} that depends
+ * on that {@link SortedList}.
+ * <li>Create a {@link JXTable} using the {@link EventTableModel} as its model.
+ * <li>Run the {@link EventListJXTableSorting#install} method to bind the
+ * {@link JXTable}'s headers to the {@link SortedList}'s {@link Comparator}.
  *
  * <p>Note that your {@link FilterPipeline} will be changed as a consequence of
  * using {@link EventListJXTableSorting}. This is due to API limitations in
  * SwingX which will hopefully be eventually resolved. For this reason it is
  * recommended that you not use {@link FilterPipeline} with this class. Instead,
- * consider using {@link ca.odell.glazedlists.FilterList} for filtering your {@link JXTable}.
+ * consider using {@link FilterList} for filtering your {@link JXTable}.
  *
  * <p>Since this class works with the <a href="https://swingx.dev.java.net/">SwingX</a>
  * project, it is likely to change as SwingX continues to evolve and mature.
  * We will attempt to maintain source and binary compatibility as SwingX changes,
  * but it is highly recommended that you test your table sorting each time you
  * update your SwingX jar file.
+ *
+ * <p>The behaviour of multiple column sorting in JXTable is not particularly
+ * flexible. For different configuration options, consider using a
+ * {@link TableComparatorChooser} instead of {@link EventListJXTableSorting}.
+ * To do this, you'll first need to disable the JXTable's built-in sorting
+ * and restore the standard header renderer:
+ * <pre><code>JXTable table = ...
+ * table.setSortable(false);
+ * table.getTableHeader().setDefaultRenderer(new JTableHeader().getDefaultRenderer());</code></pre>
  *
  * @author <a href="mailto:jesse@swank.ca">Jesse Wilson</a>
  */
@@ -62,6 +72,9 @@ public class EventListJXTableSorting {
 
     /** the original filter pipeline, used in {@link #uninstall} only */
     private final FilterPipeline originalFilterPipeline;
+
+    /** whether to sort multiple columns at a time */
+    private boolean multipleColumnSort = false;
 
     /**
      * Usually, constructors shouldn't supposed to have side-effects, but this one
@@ -84,6 +97,24 @@ public class EventListJXTableSorting {
      */
     public static EventListJXTableSorting install(JXTable table, SortedList sortedList) {
         return new EventListJXTableSorting(table, sortedList);
+    }
+
+    /**
+     * Toggle whether clicked columns will be sorted after other sorted columns
+     * (ie. break ties), or whether they will be come the exclusive sorted
+     * column.
+     *
+     * <p>The default behaviour of JXTable multiple column sorting is that each
+     * click appends a column to the sorting columns, reversing that column
+     * if it's already sorting. Clearing the sorting columns is done by holding
+     * <code>SHIFT</code> and clicking on a column header.
+     *
+     * @see org.jdesktop.swingx.JXTableHeader.SortGestureRecognizer to change
+     * the clear/click behaviour
+     * @see TableComparatorChooser for a more flexible approach to table sorting
+     */
+    public void setMultipleColumnSort(boolean multipleColumnSort) {
+        this.multipleColumnSort = multipleColumnSort;
     }
 
     /**
@@ -123,29 +154,26 @@ public class EventListJXTableSorting {
 
         /** {@inheritDoc} */
         public void toggleSortOrder(int columnIndex, Comparator comparator) {
-            List<? extends SortKey> sortKeys = getSortKeys();
+            List<SortKey> newSortKeys = new ArrayList<SortKey>(getSortKeys());
 
             // see if we're already sorting with this column
-            SortKey columnSortKey = null;
-            for(Iterator<? extends SortKey> s = sortKeys.iterator(); s.hasNext(); ) {
+            SortOrder columnSortOrder = SortOrder.ASCENDING;
+            for(Iterator<? extends SortKey> s = newSortKeys.iterator(); s.hasNext(); ) {
                 SortKey sortKey = (SortKey)s.next();
                 if(sortKey.getSortOrder() == SortOrder.UNSORTED) continue;
                 if(sortKey.getColumn() == columnIndex) {
-                    columnSortKey = sortKey;
+                    columnSortOrder = (sortKey.getSortOrder() == SortOrder.ASCENDING) ? SortOrder.DESCENDING : SortOrder.ASCENDING;
+                    s.remove();
                     break;
                 }
             }
 
-            // create the new sort key
-            if(columnSortKey == null) {
-                columnSortKey = new SortKey(SortOrder.ASCENDING, columnIndex);
-            } else {
-                SortOrder sortOrder = columnSortKey.getSortOrder() == SortOrder.ASCENDING ? SortOrder.DESCENDING : SortOrder.ASCENDING;
-                columnSortKey = new SortKey(sortOrder, columnIndex);
-            }
-
             // prepare the new sort order
-            setSortKeys(Collections.singletonList(columnSortKey));
+            if(!multipleColumnSort) newSortKeys.clear();
+            newSortKeys.add(new SortKey(columnSortOrder, columnIndex));
+
+            // kick off the sort
+            setSortKeys(newSortKeys);
         }
 
         /** {@inheritDoc} */
