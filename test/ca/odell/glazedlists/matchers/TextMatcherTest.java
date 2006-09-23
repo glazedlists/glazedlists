221e073bcdf4903ec8dca9ee8b200e3ec2820cfb
Jesse Wilson <jesse@swank.ca>
2006-09-23 21:14:31 +0000

    Now with part of removed element support for SortedList, but not all....
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1766 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/matchers/TextMatcherTest.java
+++ test/ca/odell/glazedlists/matchers/TextMatcherTest.java
@@ -522,11 +522,11 @@ public class TextMatcherTest extends TestCase {
 
 
         // the uber test ensures that diacritics and case are unimportant when comparing any of these characters
-        String überString = "ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõöùúûüýÿ";
-        list.add(überString);
+        String uberString = "\u00c0\u00c1\u00c2\u00c3\u00c4\u00c5\u00c7\u00c8\u00c9\u00ca\u00cb\u00cc\u00cd\u00ce\u00cf\u00d1\u00d2\u00d3\u00d4\u00d5\u00d6\u00d9\u00da\u00db\u00dc\u00dd\u00e0\u00e1\u00e2\u00e3\u00e4\u00e5\u00e7\u00e8\u00e9\u00ea\u00eb\u00ec\u00ed\u00ee\u00ef\u00f1\u00f2\u00f3\u00f4\u00f5\u00f6\u00f9\u00fa\u00fb\u00fc\u00fd\u00ff";
+        list.add(uberString);
         textMatcherEditor.setFilterText(new String[] {"aaaaaaceeeeiiiinooooouuuuyAAAAAACEEEEIIIINOOOOOUUUUYY"});
         assertEquals(1, list.size());
-        assertEquals(überString, list.get(0));
+        assertEquals(uberString, list.get(0));
     }
 
     /**
@@ -538,6 +538,24 @@ public class TextMatcherTest extends TestCase {
     private static class IntegerTextFilterator implements TextFilterator<Integer> {
         public void getFilterStrings(List<String> baseList, Integer element) {
             ((List) baseList).add(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        for(char i = 0; i < args[0].length(); i++) {
+            char c = args[0].charAt(i);
+
+            if(c < 128) {
+                System.out.print(c);
+                continue;
+            }
+
+            String hexString = Integer.toHexString(c);
+            while(hexString.length() < 4) hexString = "0" + hexString;
+
+            System.out.print("\\u" + hexString);
+
+
         }
     }
 }
