3795b77e8e97d3ce0546eda7177ed2f6a2ee927f
Jesse Wilson <jesse@swank.ca>
2006-09-28 05:27:35 +0000

    Starting Sorting work on TreeList
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1779 696c4f5c-992c-4387-df9e-f6c076136008

--- test/ca/odell/glazedlists/TreeListTest.java
+++ test/ca/odell/glazedlists/TreeListTest.java
@@ -612,7 +612,7 @@ public class TreeListTest extends TestCase {
         });
     }
 
-    public void testTreeSorting() {
+    public void testTreeSorting_FixMe() {
         EventList<String> source = new BasicEventList<String>();
         SortedList<String> sortedSource = new SortedList<String>(source);
         TreeList<String> treeList = new TreeList<String>(sortedSource, new CharacterTreeFormat(), (Comparator)GlazedLists.comparableComparator());
