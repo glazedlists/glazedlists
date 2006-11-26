4e69752a1122c87c27402bad781f39ca43106229
Jesse Wilson <jesse@swank.ca>
2006-11-26 08:58:09 +0000

    Cleanup Xml Browser
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@1889 696c4f5c-992c-4387-df9e-f6c076136008

---
+++ extensions/issuesbrowser/source/com/publicobject/xmlbrowser/EventListXmlContentHandler.java
@@ -0,0 +1,78 @@
+/* Glazed Lists                                                 (c) 2003-2006 */
+/* http://publicobject.com/glazedlists/                      publicobject.com,*/
+/*                                                     O'Dell Engineering Ltd.*/
+package com.publicobject.xmlbrowser;
+
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.GlazedLists;
+
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.IOException;
+
+import com.publicobject.misc.xml.SaxParserSidekick;
+
+/**
+ * Convert an XML input stream into an EventList of Elements.
+ *
+ * @author <a href="mailto:jesse@swank.ca">Jesse Wilson</a>
+ */
+class EventListXmlContentHandler extends DefaultHandler {
+
+    private final EventList<Tag> target;
+    private final List<Tag> stack = new ArrayList<Tag>();
+
+    public EventListXmlContentHandler(EventList<Tag> target) {
+        this.target = GlazedLists.threadSafeList(target);
+    }
+
+    public EventList<Tag> parse(InputStream inputStream) {
+        try {
+            XMLReader xmlReader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+            SaxParserSidekick.install(xmlReader);
+
+            EventListXmlContentHandler handler = new EventListXmlContentHandler(target);
+            xmlReader.setContentHandler(handler);
+            xmlReader.parse(new InputSource(inputStream));
+
+            return handler.target;
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        } catch(SAXException e) {
+            throw new RuntimeException(e);
+        } catch(ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+        Tag child;
+        if(!stack.isEmpty()) {
+            Tag last = stack.get(stack.size() - 1);
+            child = last.createChild(qName);
+
+        } else {
+            child = new Tag(qName);
+        }
+
+        target.add(child);
+        stack.add(child);
+    }
+
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        stack.remove(stack.size() - 1);
+    }
+
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        Tag last = stack.get(stack.size() - 1);
+        last.append(new String(ch, start, length));
+    }
+}
---
+++ extensions/issuesbrowser/source/com/publicobject/xmlbrowser/Tag.java
@@ -0,0 +1,49 @@
+package com.publicobject.xmlbrowser;
+
+/**
+ * Model an XML element.
+ *
+ * @author <a href="mailto:jesse@swank.ca">Jesse Wilson</a>
+ */
+public class Tag implements Comparable<Tag> {
+
+    private Tag parent = null;
+    private final String qName;
+    private String text = "";
+
+    public Tag(String qName) {
+        this.parent = null;
+        this.qName = qName;
+        this.text = "";
+    }
+
+    public int compareTo(Tag o) {
+        return qName.compareTo(o.qName);
+    }
+
+    public String getQName() {
+        return qName;
+    }
+
+    public Tag getParent() {
+        return parent;
+    }
+
+    public String getText() {
+        return text.trim();
+    }
+
+    public Tag createChild(String qName) {
+        Tag child = new Tag(qName);
+        child.parent = this;
+        return child;
+    }
+
+    public void append(String text) {
+        this.text += text;
+    }
+
+    public String toString() {
+        return qName;
+    }
+}
