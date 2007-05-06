27c718de87cbd0f9ab607b47ed4d7ef95ed7508f
Jesse Wilson <jesse@swank.ca>
2007-05-06 21:33:41 +0000

    KeyedCollection will help TreeList speedups
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@2040 696c4f5c-992c-4387-df9e-f6c076136008

---
+++ source/ca/odell/glazedlists/impl/adt/KeyedCollection.java
@@ -0,0 +1,33 @@
+package ca.odell.glazedlists.impl.adt;
+
+/**
+ * @author jessewilson
+ */
+public interface KeyedCollection<T> {
+
+    /**
+     * Inserts the specified value at the
+     * specified position.
+     *
+     * @returns the previous value at the
+     *      specified position.
+     */
+    T insert(Position position, T value);
+
+    /**
+     * Returns the first position that the specified
+     * value is stored at.
+     *
+     * @param min the returned value will be greater
+     *     than or equal to this.
+     * @param max the returned position will be less
+     *     than this.
+     * @returns the position, or <code>null</code> if
+     *     no such value exists in the requested range.
+     */
+    Position find(Position min, Position max, T value);
+
+    interface Position extends Comparable<Position> {
+
+    }
+}
---
+++ test/ca/odell/glazedlists/impl/adt/KeyedCollectionTest.java
@@ -0,0 +1,79 @@
+package ca.odell.glazedlists.impl.adt;
+
+import ca.odell.glazedlists.impl.adt.KeyedCollection.Position;
+
+import junit.framework.TestCase;
+
+/**
+ * @author jessewilson
+ */
+public class KeyedCollectionTest extends TestCase {
+
+    private Position a = new NamePosition("A");
+    private Position b = new NamePosition("B");
+    private Position c = new NamePosition("C");
+    private Position d = new NamePosition("D");
+    private Position e = new NamePosition("E");
+    private Position f = new NamePosition("F");
+
+    private KeyedCollection<String> collection = null;
+
+    public void testSimpleQueries() {
+        String result = collection.insert(a, "alpha");
+        assertNull(result);
+
+        assertEquals(a, collection.find(a, b, "alpha"));
+        assertNull(collection.find(a, a, "alpha"));
+        assertNull(collection.find(b, c, "alpha"));
+
+        collection.insert(b, "beta");
+        collection.insert(c, "beta");
+        assertEquals(b, collection.find(a, d, "beta"));
+        assertEquals(b, collection.find(b, d, "beta"));
+        assertEquals(c, collection.find(c, d, "beta"));
+    }
+
+    public void testHeavilyPopulatedCollection() {
+        collection.insert(a, "alpha");
+        collection.insert(b, "beta");
+        collection.insert(c, "cat");
+        collection.insert(d, "dog");
+        collection.insert(e, "elephant");
+        collection.insert(f, "firehouse");
+
+        assertEquals(a, collection.find(a, f, "alpha"));
+        assertEquals(b, collection.find(a, f, "beta"));
+        assertEquals(c, collection.find(a, f, "cat"));
+        assertEquals(d, collection.find(a, f, "dog"));
+        assertEquals(e, collection.find(a, f, "elephant"));
+        assertNull(collection.find(a, f, "firehouse"));
+    }
+
+    public void testReplacedValue() {
+        collection.insert(b, "beta");
+        assertEquals("beta", collection.insert(b, "boop"));
+        assertEquals("boop", collection.insert(b, "beep"));
+    }
+
+    public void testInconsistentMinMax() {
+        try {
+            collection.find(b, a, "alpha");
+            fail();
+        } catch(IllegalArgumentException e) {
+            // expected
+        }
+    }
+
+    static class NamePosition implements Position {
+        final String name;
+        public NamePosition(String name) {
+            this.name = name;
+        }
+        public int compareTo(Position o) {
+            return name.compareTo(((NamePosition)o).name);
+        }
+        public String toString() {
+            return name;
+        }
+    }
+}
