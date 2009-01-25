cb2ca124e7103ef8cf4beb80613058b2eb7544ce
Jesse Wilson <jesse@swank.ca>
2009-01-25 09:56:00 +0000

    Tiny tweaks to SwingCalculations for non-numbers
    git-svn-id: https://svn.java.net/svn/glazedlists~svn/trunk@2255 696c4f5c-992c-4387-df9e-f6c076136008

--- extensions/calculation/source/ca/odell/glazedlists/swing/SwingCalculations.java
+++ extensions/calculation/source/ca/odell/glazedlists/swing/SwingCalculations.java
@@ -19,9 +19,9 @@ public final class SwingCalculations {
      * it reports a change.
      *
      * @param label the JLabel displaying the value of the calculation
-     * @param calculation a source of a numeric value that changes over time
+     * @param calculation a source of a value that changes over time
      */
-    public static void bind(JLabel label, Calculation calculation) {
+    public static void bind(JLabel label, Calculation<?> calculation) {
         bind(label, calculation, null);
     }
 
@@ -30,41 +30,39 @@ public final class SwingCalculations {
      * it reports a change.
      *
      * @param label the JLabel displaying the value of the calculation
-     * @param calculation a source of a numeric value that changes over time
-     * @param formatter used to format the raw numeric value of the calculation
-     *      into pretty display text
+     * @param calculation a source of a value that changes over time
+     * @param formatter used to format the raw value of the calculation into pretty display text
      */
-    public static void bind(JLabel label, Calculation<? extends Number> calculation, Format formatter) {
+    public static void bind(JLabel label, Calculation<?> calculation, Format formatter) {
         calculation.addPropertyChangeListener(new CalculationToLabelBinder(label, formatter, calculation.getValue()));
     }
 
     /**
      * Updates the given label with the latest value of a Calculation each time
      * it reports a change. The value displayed in the label can optionally be
-     * formatted using a specified NumberFormat.
+     * formatted using a specified Format.
      */
     private static final class CalculationToLabelBinder implements PropertyChangeListener {
 
         private final JLabel label;
         private final Format formatter;
 
-        private CalculationToLabelBinder(JLabel label, Format formatter, Number initialValue) {
+        private CalculationToLabelBinder(JLabel label, Format formatter, Object initialValue) {
             this.label = label;
             this.formatter = formatter;
             update(initialValue);
         }
 
-        private void update(Number value) {
-            final String text = formatter == null ? (isNumberFormattable(value) ? String.valueOf(value) : "") : formatter.format(value);
-            label.setText(text);
-        }
-
-        private static boolean isNumberFormattable(Number n) {
-            return n != null && !n.equals(new Float(Float.NaN)) && !n.equals(new Double(Double.NaN));
+        private void update(Object value) {
+            if (formatter == null) {
+                label.setText(value != null ? String.valueOf(value) : "");
+            } else {
+                label.setText(formatter.format(value));
+            }
         }
 
         public void propertyChange(PropertyChangeEvent evt) {
-            update((Number) evt.getNewValue());
+            update(evt.getNewValue());
         }
     }
 }
