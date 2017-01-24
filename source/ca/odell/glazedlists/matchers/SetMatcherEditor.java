5f9d6983836f634b5ea7b0f49db6d89e9bfbb274
Holger Brands <holger.brands@googlemail.com>
2017-01-24 21:12:36 +0100

    Expanded tests and documentation. Fixed BLACKLIST filtering.

--- source/ca/odell/glazedlists/matchers/SetMatcherEditor.java
+++ source/ca/odell/glazedlists/matchers/SetMatcherEditor.java
@@ -3,30 +3,51 @@ package ca.odell.glazedlists.matchers;
 import static ca.odell.glazedlists.impl.Preconditions.checkNotNull;
 import static ca.odell.glazedlists.impl.Preconditions.checkState;
 
+import ca.odell.glazedlists.FunctionList.Function;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
-import ca.odell.glazedlists.FunctionList.Function;
-
+/**
+ * A {@link MatcherEditor} with blacklist or whitelist matching functionality.
+ * To do its work, SetMatcherEditor needs a match set, a function and a mode.
+ * A match set of <em>type O</em> needs to be specified as black- or whitelist. The match set can be set at any time.
+ * The specified function transforms the list elements of <em> type E</em> into <em>type O</em> for comparison with the match set.
+ * The given mode determines the behaviour of the matcher editor:
+ * <ul>
+ * <li>With {@link Mode#BLACKLIST} transformed list elements that are contained in the match set are filtered out.</li>
+ * <li>With {@link Mode#WHITELIST_EMPTY_MATCH_ALL} transformed list elements that are contained in the match set are matched.
+ * An empty match set matches all elements.</li>
+ * <li>With {@link Mode#WHITELIST_EMPTY_MATCH_NONE} transformed list elements that are contained in the match set are matched.
+ * An empty match set matches no elements.</li>
+ * </ul>
+ *
+ * @param <E> type of list elements
+ * @param <O> type of match set
+ *
+ * @see #create(Mode, Function)
+ * @see #setMatchSet(Set)
+ * @see Mode
+ */
 public final class SetMatcherEditor<E, O> extends AbstractMatcherEditor<E> {
-
-    // ~ Static fields ---------------------------------------------------------------------------------------------
 
     private static final Logger L = Logger.getLogger(SetMatcherEditor.class.toString());
 
-    // ~ Static methods --------------------------------------------------------------------------------------------
-
+    /**
+     * Creates a {@link SetMatcherEditor} with the specified {@link Mode} and transformation function.
+     * @param mode operation mode of the MatcherEditor (!= null)
+     * @param fn function to extract the value from a list element to be matched against the match set (!= null)
+     * @return the constructed MatcherEditor
+     *s
+     * @see Mode
+     */
     public static <E, O> SetMatcherEditor<E, O> create(final Mode mode, final Function<E, O> fn) {
         return new SetMatcherEditor<E, O>(mode, fn);
     }
 
-    // ~ Instance fields -------------------------------------------------------------------------------------------
-
-    private final Function<E, O> function;
-    private       Mode           mode;
-
-    // ~ Constructors ----------------------------------------------------------------------------------------------
+	private final Function<E, O> function;
+	private Mode mode;
 
     private SetMatcherEditor(final Mode mode, final Function<E, O> function) {
         this.function = checkNotNull(function);
@@ -39,8 +60,11 @@ public final class SetMatcherEditor<E, O> extends AbstractMatcherEditor<E> {
         }
     }
 
-    // ~ Methods ---------------------------------------------------------------------------------------------------
-
+    /**
+     * Sets a new match set which triggers a refiltering.
+     *
+     * @param newSet the new match set (!= null)
+     */
     public void setMatchSet(final Set<O> newSet) {
         checkNotNull(newSet);
 
@@ -68,62 +92,63 @@ public final class SetMatcherEditor<E, O> extends AbstractMatcherEditor<E> {
 
         } else if (oldSet.isEmpty()) {
             L.fine("old set was empty, new set is not -> firing change");
-            this.fireChanged(new SetMatcher<E, O>(newSet, this.function));
+            this.fireChanged(new SetMatcher<E, O>(newSet, this.mode, this.function));
 
         } else if (oldSet.containsAll(newSet)) {
 
             if (this.mode == Mode.BLACKLIST) {
                 L.fine("old set contains new set (blacklist) -> firing relaxed");
-                this.fireRelaxed(new SetMatcher<E, O>(newSet, this.function));
+                this.fireRelaxed(new SetMatcher<E, O>(newSet, this.mode, this.function));
 
             } else {
                 L.fine("old set contains new set (whitelist) -> firing constrained");
-                this.fireConstrained(new SetMatcher<E, O>(newSet, this.function));
+                this.fireConstrained(new SetMatcher<E, O>(newSet, this.mode, this.function));
             }
 
         } else if (newSet.containsAll(oldSet)) {
             if (this.mode == Mode.BLACKLIST) {
                 L.fine("new set contains old set (blacklist) -> firing constrained");
-                this.fireConstrained(new SetMatcher<E, O>(newSet, this.function));
+                this.fireConstrained(new SetMatcher<E, O>(newSet, this.mode, this.function));
 
             } else {
                 L.fine("new set contains old set (whitelist) -> firing relaxed");
-                this.fireRelaxed(new SetMatcher<E, O>(newSet, this.function));
+                this.fireRelaxed(new SetMatcher<E, O>(newSet, this.mode, this.function));
             }
 
         } else {
             L.fine("old and new set differ -> firing change");
-            this.fireChanged(new SetMatcher<E, O>(newSet, this.function));
+            this.fireChanged(new SetMatcher<E, O>(newSet, this.mode, this.function));
         }
     }
 
-    // ~ Enumerations ----------------------------------------------------------------------------------------------
-
+    /**
+     * Supported modes of operation for the MatcherEditor.
+     */
     public enum Mode {
-        BLACKLIST, WHITELIST_EMPTY_MATCH_NONE, WHITELIST_EMPTY_MATCH_ALL
+    	/** the match set specifies the elements to be filtered out. */
+        BLACKLIST,
+        /** the match set specifies the elements to be matched. The empty match set matches none. */
+        WHITELIST_EMPTY_MATCH_NONE,
+        /** the match set specifies the elements to be matched. The empty match set matches all. */
+        WHITELIST_EMPTY_MATCH_ALL
     }
-
-    // ~ Inner Classes ---------------------------------------------------------------------------------------------
 
     private final static class SetMatcher<E, O> implements Matcher<E> {
 
-        // ~ Instance fields -------------------------------------------------------------------------------------------
+		private final Set<O> matchSet;
+		private final Function<E, O> fn;
+		private final Mode mode;
 
-        private final Set<O> matchSet;
-        private final Function<E, O>  fn;
-
-        // ~ Constructors ----------------------------------------------------------------------------------------------
-
-        private SetMatcher(final Set<O> matchSet, final Function<E, O> fn) {
+        private SetMatcher(final Set<O> matchSet, final Mode mode, final Function<E, O> fn) {
             this.matchSet = new HashSet<O>(matchSet);
+            this.mode = mode;
             this.fn = checkNotNull(fn);
         }
 
-        // ~ Methods ---------------------------------------------------------------------------------------------------
-
         @Override
         public boolean matches(final E input) {
-            return this.matchSet.contains(this.fn.evaluate(input));
+            boolean result = this.matchSet.contains(this.fn.evaluate(input));
+            return (mode == mode.BLACKLIST) ? !result : result;
         }
     }
 }
--- test/ca/odell/glazedlists/matchers/SetMatcherEditorTest.java
+++ test/ca/odell/glazedlists/matchers/SetMatcherEditorTest.java
@@ -1,56 +1,84 @@
 package ca.odell.glazedlists.matchers;
 
+import static org.junit.Assert.assertEquals;
+
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.FilterList;
+import ca.odell.glazedlists.FunctionList.Function;
+import ca.odell.glazedlists.GlazedLists;
+import ca.odell.glazedlists.matchers.SetMatcherEditor.Mode;
+
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import ca.odell.glazedlists.FunctionList.Function;
-import ca.odell.glazedlists.matchers.SetMatcherEditor.Mode;
-
 public class SetMatcherEditorTest {
+
+	private Date july2007 = new Date(1183248000000L);
+	private Date july2008 = new Date(1214870400000L);
+	private Date july2009 = new Date(1246406400000L);
+
+	private EventList<Date> source;
+
+	@Before
+	public void setup() {
+		source = GlazedLists.eventListOf(july2007, july2008, july2009);
+		assertEquals(2007, july2007.getYear() + 1900);
+		assertEquals(2008, july2008.getYear() + 1900);
+		assertEquals(2009, july2009.getYear() + 1900);
+	}
 
     @Test
     public void testWhiteListEmptyMatchNone() {
         final SetMatcherEditor<Date, Integer> matcherEditor = SetMatcherEditor.create(Mode.WHITELIST_EMPTY_MATCH_NONE, new Function<Date, Integer>() {
             @Override
             public Integer evaluate(Date date) {
-                return date.getYear();
+                return date.getYear() + 1900;
             }
         });
-
+        EventList<Date> filterList = new FilterList<Date>(source, matcherEditor);
         final CountingMatcherEditorListener<Date> counter = new CountingMatcherEditorListener<Date>();
         matcherEditor.addMatcherEditorListener(counter);
         counter.assertCounterState(0, 0, 0, 0, 0);
+        assertEquals(Arrays.asList(), filterList);
 
         // give some initial range (changed)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007)));
         counter.assertCounterState(0, 0, 1, 0, 0);
+        assertEquals(Arrays.asList(july2007), filterList);
 
         // relax (more values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007, 2008)));
         counter.assertCounterState(0, 0, 1, 0, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
 
         // constrain (less values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007)));
         counter.assertCounterState(0, 0, 1, 1, 1);
+        assertEquals(Arrays.asList(july2007), filterList);
 
         // changed (different values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(0, 0, 2, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
 
         // empty set
         matcherEditor.setMatchSet(new HashSet<Integer>());
         counter.assertCounterState(0, 1, 2, 1, 1);
+        assertEquals(Arrays.asList(), filterList);
 
         // changed (different values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(0, 1, 3, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
 
         // test changing nothing
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(0, 1, 3, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
     }
 
     @Test
@@ -58,41 +86,49 @@ public class SetMatcherEditorTest {
         final SetMatcherEditor<Date, Integer> matcherEditor = SetMatcherEditor.create(Mode.WHITELIST_EMPTY_MATCH_ALL, new Function<Date, Integer>() {
             @Override
             public Integer evaluate(Date date) {
-                return date.getYear();
+                return date.getYear() + 1900;
             }
         });
-
+        EventList<Date> filterList = new FilterList<Date>(source, matcherEditor);
         final CountingMatcherEditorListener<Date> counter = new CountingMatcherEditorListener<Date>();
         matcherEditor.addMatcherEditorListener(counter);
         counter.assertCounterState(0, 0, 0, 0, 0);
+        assertEquals(Arrays.asList(july2007, july2008, july2009), filterList);
 
         // give some initial range (changed)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007)));
         counter.assertCounterState(0, 0, 1, 0, 0);
+        assertEquals(Arrays.asList(july2007), filterList);
 
         // relax (more values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007, 2008)));
         counter.assertCounterState(0, 0, 1, 0, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
 
         // constrain (less values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007)));
         counter.assertCounterState(0, 0, 1, 1, 1);
+        assertEquals(Arrays.asList(july2007), filterList);
 
         // changed (different values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(0, 0, 2, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
 
         // empty set
         matcherEditor.setMatchSet(new HashSet<Integer>());
         counter.assertCounterState(1, 0, 2, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008, july2009), filterList);
 
         // changed (different values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(1, 0, 3, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
 
         // test changing nothing
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(1, 0, 3, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008), filterList);
     }
 
     @Test
@@ -100,40 +136,48 @@ public class SetMatcherEditorTest {
         final SetMatcherEditor<Date, Integer> matcherEditor = SetMatcherEditor.create(Mode.BLACKLIST, new Function<Date, Integer>() {
             @Override
             public Integer evaluate(Date date) {
-                return date.getYear();
+                return date.getYear() + 1900;
             }
         });
-
+        EventList<Date> filterList = new FilterList<Date>(source, matcherEditor);
         final CountingMatcherEditorListener<Date> counter = new CountingMatcherEditorListener<Date>();
         matcherEditor.addMatcherEditorListener(counter);
         counter.assertCounterState(0, 0, 0, 0, 0);
+        assertEquals(Arrays.asList(july2007, july2008, july2009), filterList);
 
         // give some initial range (changed)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007)));
         counter.assertCounterState(0, 0, 1, 0, 0);
+        assertEquals(Arrays.asList(july2008, july2009), filterList);
 
         // constrain (more values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007, 2008)));
         counter.assertCounterState(0, 0, 1, 1, 0);
+        assertEquals(Arrays.asList(july2009), filterList);
 
         // relax (less values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2006, 2007)));
         counter.assertCounterState(0, 0, 1, 1, 1);
+        assertEquals(Arrays.asList(july2008, july2009), filterList);
 
         // changed (different values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(0, 0, 2, 1, 1);
+        assertEquals(Arrays.asList(july2009), filterList);
 
         // none
         matcherEditor.setMatchSet(new HashSet<Integer>());
         counter.assertCounterState(1, 0, 2, 1, 1);
+        assertEquals(Arrays.asList(july2007, july2008, july2009), filterList);
 
         // changed (different values)
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(1, 0, 3, 1, 1);
+        assertEquals(Arrays.asList(july2009), filterList);
 
         // test changing nothing
         matcherEditor.setMatchSet(new HashSet<Integer>(Arrays.asList(2007, 2008)));
         counter.assertCounterState(1, 0, 3, 1, 1);
+        assertEquals(Arrays.asList(july2009), filterList);
     }
 }
