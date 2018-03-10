f83234ac2c61304e7dd6b4e06a4e09daa4785b73
Rob Eden <rob@starlight-systems.com>
2018-03-09 22:23:49 -0600

    Disable SWT tests on Mac since they explode with "Invalid thread access"

--- extensions/swt/src/test/java/ca/odell/glazedlists/swt/SwtClassRule.java
+++ extensions/swt/src/test/java/ca/odell/glazedlists/swt/SwtClassRule.java
@@ -5,6 +5,7 @@ package ca.odell.glazedlists.swt;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.junit.Assume;
 import org.junit.ClassRule;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
@@ -25,6 +26,35 @@ public class SwtClassRule implements TestRule {
      */
     @Override
     public Statement apply(final Statement base, Description description) {
+        ////////////////////
+        // Disabling SWT tests on Mac because they explode due to these errors: (RE - 2018-03-09)
+        //
+        //   ***WARNING: Display must be created on main thread due to Cocoa restrictions.
+        //
+        //   org.eclipse.swt.SWTException: Invalid thread access
+        //
+        //       at org.eclipse.swt.SWT.error(Unknown Source)
+        //       at org.eclipse.swt.SWT.error(Unknown Source)
+        //       at org.eclipse.swt.SWT.error(Unknown Source)
+        //       at org.eclipse.swt.widgets.Display.error(Unknown Source)
+        //       at org.eclipse.swt.widgets.Display.createDisplay(Unknown Source)
+        //       at org.eclipse.swt.widgets.Display.create(Unknown Source)
+        //       at org.eclipse.swt.graphics.Device.<init>(Unknown Source)
+        //       at org.eclipse.swt.widgets.Display.<init>(Unknown Source)
+        //       at org.eclipse.swt.widgets.Display.<init>(Unknown Source)
+        //       at org.eclipse.swt.widgets.Display.getDefault(Unknown Source)
+        //       at ca.odell.glazedlists.swt.SwtClassRule$SwtHelper.init(SwtClassRule.java:69)
+        //       at ca.odell.glazedlists.swt.SwtClassRule$1.evaluate(SwtClassRule.java:35)
+        //       at org.junit.rules.RunRules.evaluate(RunRules.java:20)
+        //       at org.junit.runners.ParentRunner.run(ParentRunner.java:309)
+        //       at org.junit.runner.JUnitCore.run(JUnitCore.java:160)
+        //       at com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)
+        //       at com.intellij.rt.execution.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:47)
+        //       at com.intellij.rt.execution.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:242)
+        //       at com.intellij.rt.execution.junit.JUnitStarter.main(JUnitStarter.java:70)
+        Assume.assumeFalse( "SWT tests are not supported on Mac",
+            System.getProperty("os.name").toLowerCase().startsWith("mac"));
+
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
